cd732cd6d68c007c38d312f0fa4481a5f3db210d
Zake <zake@steelrabbit.com>
2024-05-23 00:14:54 -0700

    code framework and user management

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+import os
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chatbot83.db'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
+
+from app import routes, models
